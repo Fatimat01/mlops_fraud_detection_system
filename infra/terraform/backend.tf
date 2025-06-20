269f04f09dee124f0c142627632fa378008c175b
Fatimat Atanda <atandafatimat01@gmail.com>
2025-06-19 21:24:20 -0500

    backend key rename

--- infra/terraform/backend.tf
+++ infra/terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "fatimat-tf-state"
-    key            = "terraform.tfstate"
+    key            = "fraud-detection.tfstate"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "fatimat-tf-state-lock"
